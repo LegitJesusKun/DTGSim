d70f5bc80a45cd0fabcd79bbed80d5561e48ef83
CameronDohertySelwyn <65873545+CameronDohertySelwyn@users.noreply.github.com>
2022-02-25 15:05:13 +1300

    Please finish it for us nadav

---
+++ DTG Simulator/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    int Efficiency = 1;
+    int Morale = 1;
+    int Art = 1;
+    int Coding = 1; 
+    // Start is called before the first frame update    
+    void Start()
+    {
+
+        Debug.Log(Coding);
+
+        Debug.Log(Art);
+
+        Debug.Log(Morale);
+
+        Debug.Log(Efficiency);
+
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
